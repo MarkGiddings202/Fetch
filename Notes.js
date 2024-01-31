d1be34bd3fbb141288e30a2cc36451b322af677d
MarkGiddings202 <gavingiddings1997@gmail.com>
2024-01-30 23:42:48 -0500

    fetch

--- Notes.js
+++ Notes.js
@@ -1,5 +1,5 @@
 // Fetch Example
-import fetch from "node-fetch"
+// import fetch from "node-fetch"
 
 const response_linkedIn = fetch(
   "https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit"
@@ -40,9 +40,9 @@ response_wallHaven.then((result) => {
     */
   }
 
-// Checking for response_wallHaven objects prototype
-// console.log(result.__proto__);
-/*
+  // Checking for response_wallHaven objects prototype
+  // console.log(result.__proto__);
+  /*
     Object [Response] {
   type: [Getter],
   url: [Getter],
@@ -62,5 +62,23 @@ response_wallHaven.then((result) => {
 }
     */
 
-console.log(result.__proto__.__proto__);
+  // console.log(result.url);
+  // console.log(result.headers);
+  // console.log(result.body);
+  // console.log(result.ok);
+  // console.log(result.status);
+  /*
+   */
+  // Getting the text from the webpage
+  console.log(result.text()); // result.text() returns us a promise
+  // Since this is a promise we can use a .then() on it to receive that promise
+  // first we store it to a variable
+  let text = result.text();
+  // we then refer to that text & start a .then() chain on it
+  text.then((html) => {
+    console.log(html);
+  }).catch((error) => {
+    // Handle any errors that occurred during the fetch
+    console.error('Fetch error:', error);
+  });;
 });
